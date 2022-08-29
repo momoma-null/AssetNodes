6e73344b2066e18abeda6f34654b01f3d7ac1e50
momoma <momoma.creation@gmail.com>
2022-08-29 17:23:40 +0900

    Add input port for directory name

--- AssetNodes/Editor/Nodes/Path/GetChildDirectoryNode.cs
+++ AssetNodes/Editor/Nodes/Path/GetChildDirectoryNode.cs
@@ -10,26 +10,25 @@ namespace MomomaAssets.GraphView.AssetNodes
     [CreateElement(typeof(AssetNodesGUI), "Path/Get Child Directory")]
     sealed class GetChildDirectoryNode : INodeProcessor
     {
-        [SerializeField]
-        string m_DirectoryName = string.Empty;
-
         public Color HeaderColor => ColorDefinition.PathNode;
 
         GetChildDirectoryNode() { }
 
         public void Initialize(IPortDataContainer portDataContainer)
         {
-            portDataContainer.AddInputPort(PathDataPortDefinition.Default);
+            portDataContainer.AddInputPort(PathDataPortDefinition.Default, "Current Directory");
+            portDataContainer.AddInputPort(PathDataPortDefinition.Default, "Child Directory");
             portDataContainer.AddOutputPort(PathDataPortDefinition.Default);
         }
 
         public void Process(IProcessingDataContainer container)
         {
             var pathData = container.GetInput(0, PathDataPortDefinition.Default);
+            var directory = container.GetInput(1, PathDataPortDefinition.Default);
             var outPathData = new PathData(asset =>
             {
                 var path = pathData.GetPath(asset);
-                return Path.Combine(path, m_DirectoryName);
+                return Path.Combine(path, directory.GetPath(asset));
             });
             container.SetOutput(0, outPathData);
         }
