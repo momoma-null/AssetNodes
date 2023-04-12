26bdad3cb3241a1a6d904c843c6d1c5a6417d685
momoma <momoma.creation@gmail.com>
2023-04-12 22:10:56 +0900

    Remove unused method

--- AssetNodes/Editor/PathData.cs
+++ AssetNodes/Editor/PathData.cs
@@ -8,14 +8,7 @@ namespace MomomaAssets.GraphView.AssetNodes
 {
     public sealed class PathData : IProcessingData
     {
-        public static Func<IEnumerable<PathData>, PathData> combine = pathDatas =>
-        {
-            foreach (var i in pathDatas)
-                return new PathData(i.getPath);
-            return new PathData(asset => string.Empty);
-        };
-
-        Func<AssetData, string> getPath;
+        readonly Func<AssetData, string> getPath;
 
         public PathData(Func<AssetData, string> getPath)
         {
